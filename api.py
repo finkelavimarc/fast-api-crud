b82f6c498ad9c414736de6211fbf601a669a9ce4
finkelavimarc <finkel_avimarc@yahoo.com>
2024-03-22 18:39:36 +0200

    Update api.py

--- api.py
+++ api.py
@@ -6,36 +6,37 @@ from auth.crypto_context import CryptoContext
 from src.util import Middleware
 
 app = FastAPI()
-db = Mongo()
-crypto_context = CryptoContext()
-middle = Middleware(db=db, crypto_context=crypto_context)
 
+class AppDependencies:
+    def __init__(self):
+        self.db = Mongo()
+        self.crypto_context = CryptoContext()
+        self.middle = Middleware(db=self.db, crypto_context=self.crypto_context)
+
+dependencies = AppDependencies()
 
 @app.post("/user/signup", tags=["auth"])
 async def create_user(user: UserRegister = Depends()) -> dict:
-    result = await db.add_user(user)
+    result = await dependencies.db.add_user(user)
     return result
-
 
 @app.post("/token", response_model=Token, tags=["auth"])
 async def login_for_access_token(
     form_data: OAuth2PasswordRequestForm = Depends(),
 ) -> dict:
-    res = await middle.login_handle(form_data.username, form_data.password)
+    res = await dependencies.middle.login_handle(form_data.username, form_data.password)
     return res
-
 
 @app.get("/run", tags=["app"])
 async def get_main_data(
-    current_user: User = Depends(middle.get_current_active_user),
+    current_user: User = Depends(dependencies.middle.get_current_active_user),
 ) -> dict:
-    return {"message": "Hello World"}
-
+    return {"message": "Getting main data"}
 
 @app.get("/me", tags=["app"])
 async def get_extracted_token(
-    request: Request, current_user: User = Depends(middle.get_current_active_user)
+    request: Request, current_user: User = Depends(dependencies.middle.get_current_active_user)
 ) -> dict:
     token = request.headers["authorization"].split(" ")[1]
-    payload = middle.decode_jwt_token(token)
+    payload = dependencies.middle.decode_jwt_token(token)
     return {"extracted_data": payload}
