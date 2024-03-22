494efd06cf8932c2c538b8f209dc7dfab32a0f34
finkelavimarc <finkel_avimarc@yahoo.com>
2024-03-22 18:41:35 +0200

    Update auth_interface.py

--- auth/auth_interface.py
+++ auth/auth_interface.py
@@ -3,8 +3,10 @@ from abc import ABC, abstractmethod
 
 class Authentication(ABC):
     @abstractmethod
-    def verify_password(plain_password: str, hashed_password: str):
+    def verify_password(self, plain_password: str, hashed_password: str):
         pass
 
+    @staticmethod
+    @abstractmethod
     def get_password_hash(plain_password: str):
         pass
