eb4e64f36310760425117a71a770aee433b4ca28
finkelavimarc <finkel_avimarc@yahoo.com>
2024-03-22 18:33:31 +0200

    Update util.py

--- src/util.py
+++ src/util.py
@@ -1,4 +1,3 @@
-from jose import jwt, JWTError
 from auth.crypto_context import CryptoContext
 from fastapi import Depends, HTTPException, status
 from datetime import datetime, timedelta
@@ -28,29 +27,46 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
-    user = await Middleware._get_user(username=token_data.username)
+    user = await Middleware.get_user(username=token_data.username)
     if user is None:
         raise credentials_exception
     return user
 
 
 class Middleware:
-    db = Mongo()
+    def __init__(self, db: Mongo, crypto_context: CryptoContext):
+        self.db = db
+        self.crypto_context = crypto_context
 
-    def __init__(self):
-        self.crypto_context = CryptoContext()
-
-    async def _authenticate_user(self, username: str, password: str):
-        user = await self._get_user(username)
+    async def authenticate_user(self, username: str, password: str):
+        user = await self.get_user(username)
         if not user:
             return False
         if not self.crypto_context.verify_password(password, user["password"]):
             return False
         return user
 
-    def _create_access_token(
-        self, data: dict, expires_delta: Optional[timedelta] = None
-    ):
+    async def login_handle(self, username, plain_password) -> dict:
+        try:
+            user = await self.authenticate_user(username, plain_password)
+            if not user:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Incorrect username or password",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+            access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+            access_token = self.create_access_token(
+                data={"sub": user["username"]}, expires_delta=access_token_expires
+            )
+            return {"access_token": access_token, "token_type": "bearer"}
+        except Exception as e:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"{e}",
+            )
+
+    def create_access_token(self, data: dict, expires_delta: Optional[timedelta] = None):
         to_encode = data.copy()
         if expires_delta:
             expire = datetime.utcnow() + expires_delta
@@ -61,37 +77,6 @@ class Middleware:
         return encoded_jwt
 
     @staticmethod
-    async def _get_user(username: str):
+    async def get_user(username: str):
         user = await Middleware.db.get_user(username)
         return user
-
-    async def login_handle(self, username, plain_password) -> dict:
-        try:
-            user = await self._authenticate_user(username, plain_password)
-            if not user:
-                raise HTTPException(
-                    status_code=status.HTTP_401_UNAUTHORIZED,
-                    detail="Incorrect username or password",
-                    headers={"WWW-Authenticate": "Bearer"},
-                )
-            access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-            access_token = self._create_access_token(
-                data={"sub": user["username"]}, expires_delta=access_token_expires
-            )
-            return {"access_token": access_token, "token_type": "bearer"}
-        except Exception as e:
-            raise HTTPException(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                detail=f"{e}",
-            )
-
-    def decode_jwt_token(self, token: str = Depends(oauth2_scheme)):
-        res = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        return ExtractToken(**res)
-
-    async def get_current_active_user(
-        self, current_user: User = Depends(get_current_user)
-    ):
-        if not current_user:
-            raise HTTPException(status_code=400, detail="Inactive user")
-        return current_user
